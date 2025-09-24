e4917652da6014d40529a21e494ec971048a4f68
Jeannie Gonzalez <148590124+jeangzlez@users.noreply.github.com>
2025-09-23 17:29:50 -0700

    testing code that might open csv files in dataset

--- project_1.py
+++ project_1.py
@@ -1,2 +1,15 @@
+import glob
 import pandas as pd
-import os
+
+folder_path = "dataset"
+csv_files = glob.glob(folder_path + "/*.csv")
+
+data_frame = pd.DataFrame()
+content = []
+
+for filename in csv_files:
+    df = pd.read_csv(filename, , index_col=None)
+    content.append(df)
+
+data_fram = pd.concat(content)
+print(data_frame)
