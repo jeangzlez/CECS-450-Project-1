7f8678ae14dff22ba02834a3878974c2e81655ad
Jeannie Gonzalez <148590124+jeangzlez@users.noreply.github.com>
2025-09-30 22:40:56 -0700

    updating

--- project_1.py
+++ project_1.py
@@ -1,16 +1,4 @@
-import glob
 import pandas as pd
-import folium
+import matplotlib.pyplot as pyplot
 
-folder_path = "dataset"
-csv_files = glob.glob(folder_path + "/*.csv")
-
-data_frame = pd.DataFrame()
-content = []
-
-for filename in csv_files:
-    df = pd.read_csv(filename, index_col=None)
-    content.append(df)
-
-data_frame = pd.concat(content)
-print(data_frame)
+df = pd.read_csv("dataset/drimpair.csv")
