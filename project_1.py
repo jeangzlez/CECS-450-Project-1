f726f960f39b64baf0344b9d419e9a475984b1ca
Jeannie Gonzalez <148590124+jeangzlez@users.noreply.github.com>
2025-10-05 14:00:23 -0700

    editing comments for clarification of work

--- project_1.py
+++ project_1.py
@@ -5,19 +5,20 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("dataset/drimpair.csv")
 cf = pd.read_csv("dataset/accident.csv")
 
-#filter to California
+# filter to California only
 ca = cf[(cf["STATENAME"] == "CALIFORNIA") | (cf["STATE"] == 6)]
 
+# highways selected from groupmate's findings
 highways = ["I-5", "I-10", "I-405", "US-101", "I-110", "I-105", "I-605", "I-710"]
-#checks fatalities in highways
+# checks amount of fatalities in highways
 fatal_acc = ca[ca["TWAY_ID"].isin(highways) & (ca["FATALS"] > 0)]
-#matches case number to each other from diff csv files
+# matches case number from diff csv files
 match = df[df["ST_CASE"].isin(fatal_acc["ST_CASE"])]
 
-#merges corresponding data from csv files
+# merges corresponding data from csv files
 merged_data = match.merge(fatal_acc[["ST_CASE", "TWAY_ID"]], on="ST_CASE", how="inner")
 
-#piecharts
+# piecharts
 for hw in highways:
     subpies = merged_data[merged_data["TWAY_ID"] == hw]
     if subpies.empty:
@@ -25,7 +26,7 @@ for hw in highways:
     counts = subpies["DRIMPAIRNAME"].value_counts().reset_index()
     counts.columns = ["Drug Type", "Count"]
 
-    #customization for piecharts
+    # customization for piecharts
     plt.figure(figsize=(7, 7))
     plt.pie(
         counts["Count"],
@@ -37,7 +38,7 @@ for hw in highways:
         wedgeprops={'edgecolor': 'white', 'linewidth': 3}
     )
 
-    
+
     plt.title(
         #f"Drug Impairment in Fatal Accidents on {hw}"
         f"{hw}",
