f5827dc7481227deef6cde0afcdf8a0218b46ad1
Jeannie Gonzalez <148590124+jeangzlez@users.noreply.github.com>
2025-10-05 15:54:03 -0700

    fixing piecharts

--- project_1.py
+++ project_1.py
@@ -1,48 +1,56 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#<---------- FILTERING DATA -------->
+
 # read files
 df = pd.read_csv("dataset/drimpair.csv")
 cf = pd.read_csv("dataset/accident.csv")
 
-# filter to California only
+# filter accident.csv to California only
 ca = cf[(cf["STATENAME"] == "CALIFORNIA") | (cf["STATE"] == 6)]
 
-# highways selected from groupmate's findings
+# selected highways in California (from groupmate findings)
 highways = ["I-5", "I-10", "I-405", "US-101", "I-110", "I-105", "I-605", "I-710"]
-# checks amount of fatalities in highways
+# amount of fatal accidents from selected highways
 fatal_acc = ca[ca["TWAY_ID"].isin(highways) & (ca["FATALS"] > 0)]
-# matches case number from diff csv files
+# finds matching case numbers from csv files
 match = df[df["ST_CASE"].isin(fatal_acc["ST_CASE"])]
 
 # merges corresponding data from csv files
 merged_data = match.merge(fatal_acc[["ST_CASE", "TWAY_ID"]], on="ST_CASE", how="inner")
 
-# piecharts
+#<---------- PIECHART PLOT ---------->
+
+# creates a grid that fits 8 piecharts
+fig, axes = plt.subplots(2, 4, figsize=(7, 7))
+axes = axes.flatten()
+# tracks grid to avoid overlaps/rewriting
+index = 0
+
+# loops through selected highways
 for hw in highways:
     subpies = merged_data[merged_data["TWAY_ID"] == hw]
     if subpies.empty:
         continue
+
     counts = subpies["DRIMPAIRNAME"].value_counts().reset_index()
     counts.columns = ["Drug Type", "Count"]
 
-    # customization for piecharts
-    plt.figure(figsize=(7, 7))
-    plt.pie(
+    # design for each piechart/slices
+    axes[index].pie(
         counts["Count"],
         labels=counts["Drug Type"],
-        autopct="%1.1f%%",
+        autopct="%1.1f",
         startangle=90,
-        textprops={'fontsize': 11},
-        colors=['tomato', 'cornflowerblue', 'gold', 'orchid', 'green', 'indianred', 'darkblue' ],
-        wedgeprops={'edgecolor': 'white', 'linewidth': 3}
+        colors=['tomato', 'cornflowerblue', 'gold', 'orchid', 'green', 'indianred', 'darkblue'],
+        wedgeprops={'edgecolor': 'white', 'linewidth': 2}
     )
+    # title for each piechart
+    axes[index].set_title(f"{hw}")
+    # increment index to avoid revisiting same grid location
+    index += 1
 
-
-    plt.title(
-        #f"Drug Impairment in Fatal Accidents on {hw}"
-        f"{hw}",
-        fontdict={"fontsize": 18},
-        pad=20
-    )
-    plt.show()
+# title for entire grid
+fig.suptitle("Types of Drug Impairment reported")
+plt.show()
